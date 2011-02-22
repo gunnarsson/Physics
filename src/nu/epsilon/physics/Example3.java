934d033d080d6270095881dede9ecb02985f0474
Pär Sikö <per.siko@epsilon.nu>
2011-02-22 21:10:34 +0100

    No idea what I've done

--- src/nu/epsilon/physics/Example3.java
+++ src/nu/epsilon/physics/Example3.java
@@ -4,10 +4,13 @@ import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.shape.Shape;
 import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
+import org.anddev.andengine.sensor.accelerometer.AccelerometerData;
+import org.anddev.andengine.sensor.accelerometer.IAccelerometerListener;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
-public class Example3 extends Example2 {
+public class Example3 extends Example2 implements IAccelerometerListener{
 
 	@Override
 	public Scene onLoadScene() {
@@ -17,7 +20,7 @@ public class Example3 extends Example2 {
 		final Shape roof = new Rectangle(0, 0, CAMERA_WIDTH, 2);
 		final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
 		final Shape right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT);
-
+		
 		PhysicsFactory.createBoxBody(this.physicsWorld, ground, BodyType.StaticBody, this.fixtureDefinition);
 		PhysicsFactory.createBoxBody(this.physicsWorld, roof, BodyType.StaticBody, this.fixtureDefinition);
 		PhysicsFactory.createBoxBody(this.physicsWorld, left, BodyType.StaticBody, this.fixtureDefinition);
@@ -28,7 +31,16 @@ public class Example3 extends Example2 {
 		scene.getBottomLayer().addEntity(left);
 		scene.getBottomLayer().addEntity(right);
 
+		this.enableAccelerometerSensor(this);
+		
 		return scene;
 	}
 
+	@Override
+	public void onAccelerometerChanged(
+			final AccelerometerData pAccelerometerData) {
+		this.physicsWorld.setGravity(new Vector2(4 * pAccelerometerData.getY(),
+				4 * pAccelerometerData.getX()));
+	}
+	
 }
