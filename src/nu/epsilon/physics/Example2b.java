c1d9fe0fc264dbe73d0abfc0179e72e1439a86b5
Martin Gunnarsson <martin.gunnarsson@epsilon.nu>
2011-03-03 21:40:43 +0100

    Updated examples for Codemotion.

--- src/nu/epsilon/physics/Example2b.java
+++ src/nu/epsilon/physics/Example2b.java
@@ -8,48 +8,68 @@ import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolic
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.sprite.TiledSprite;
+import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
+import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
+import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
-public class Example1 extends BaseGameActivity {
+import android.hardware.SensorManager;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+public class Example2b extends BaseGameActivity {
 	
 	protected static final int CAMERA_WIDTH = 480;
 	protected static final int CAMERA_HEIGHT = 320;
-	private static final int BLOCKS = 7;
+	protected static final int BLOCKS = 7;
 	
-	private Texture texture;
-	private TiledTextureRegion tileRegions;
+	protected Texture texture;
+	protected TiledTextureRegion tileRegions;
+	
+	protected PhysicsWorld physicsWorld;
+	protected FixtureDef fixtureDefinition = PhysicsFactory.createFixtureDef(10, 0.5f, 0.5f);
 	
 	@Override
 	public Engine onLoadEngine() {
 		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		final RatioResolutionPolicy ratio = new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT);
+		final RatioResolutionPolicy ratio = new RatioResolutionPolicy(camera.getWidth(), camera.getHeight());
 		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, ratio, camera));
 	}
 	
 	@Override
 	public void onLoadResources() {
 		this.texture = new Texture(256, 32, TextureOptions.BILINEAR);
-		this.tileRegions = TextureRegionFactory.createTiledFromAsset(this.texture, this, "gfx/blocks.png", 0, 0,
-				BLOCKS + 1, 1);
+		this.tileRegions = TextureRegionFactory.createTiledFromAsset(this.texture, this, "gfx/blocks.png", 0, 0, BLOCKS+1,
+				1);
 		this.mEngine.getTextureManager().loadTexture(this.texture);
 	}
 	
 	@Override
 	public Scene onLoadScene() {
-		final Scene scene = new Scene(2);
+		final Scene scene = new Scene(1);
 		scene.setBackground(new ColorBackground(0, 0, 0));
+		
+		this.physicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
 		
 		// Add blocks
 		for (int i = 0; i < BLOCKS; i++) {
 			TiledSprite sprite = new TiledSprite(68 + i * 52, 144, this.tileRegions.clone());
 			sprite.setCurrentTileIndex(i);
 			scene.getTopLayer().addEntity(sprite);
+			
+			sprite.setUpdatePhysics(false);
+			Body body = PhysicsFactory.createBoxBody(physicsWorld, sprite, BodyType.DynamicBody, fixtureDefinition);
+			physicsWorld.registerPhysicsConnector(new PhysicsConnector(sprite, body));
 		}
 		
+		scene.registerUpdateHandler(physicsWorld);
 		return scene;
 	}
 	
@@ -57,5 +77,4 @@ public class Example1 extends BaseGameActivity {
 	public void onLoadComplete() {
 		
 	}
-	
 }
--- src/nu/epsilon/physics/Example4.java
+++ src/nu/epsilon/physics/Example4.java
@@ -28,99 +28,86 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 
-public class Example4 extends BaseGameActivity implements
-		IAccelerometerListener {
-
+public class Example4 extends BaseGameActivity implements IAccelerometerListener {
+	
 	protected static final int CAMERA_WIDTH = 480;
 	protected static final int CAMERA_HEIGHT = 320;
-	protected static final int BLOCKS = 6;
-
-protected Texture texture;
-protected TiledTextureRegion tileRegions;
-
+	protected static final int BLOCKS = 7;
+	
+	protected Texture texture;
+	protected TiledTextureRegion tileRegions;
+	
 	protected PhysicsWorld physicsWorld;
-	protected FixtureDef fixtureDefinition = PhysicsFactory.createFixtureDef(
-			10, 0.5f, 0.1f);
-
+	protected FixtureDef fixtureDefinition = PhysicsFactory.createFixtureDef(10, 0.5f, 0.1f);
+	
 	@Override
 	public Engine onLoadEngine() {
 		final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-		final RatioResolutionPolicy ratio = new RatioResolutionPolicy(
-				camera.getWidth(), camera.getHeight());
-		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
-				ratio, camera));
+		final RatioResolutionPolicy ratio = new RatioResolutionPolicy(camera.getWidth(), camera.getHeight());
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, ratio, camera));
 	}
-
+	
 	@Override
 	public void onLoadResources() {
 		this.texture = new BuildableTexture(256, 32, TextureOptions.BILINEAR);
-		this.tileRegions = TextureRegionFactory.createTiledFromAsset(
-				this.texture, this, "gfx/jfokus.png", 0, 0, BLOCKS + 1, 1);
+		this.tileRegions = TextureRegionFactory.createTiledFromAsset(this.texture, this, "gfx/blocks.png", 0, 0,
+				BLOCKS + 1, 1);
 		this.mEngine.getTextureManager().loadTexture(this.texture);
-
+		
 		this.enableAccelerometerSensor(this);
 	}
-
+	
 	@Override
 	public Scene onLoadScene() {
-		this.physicsWorld = new PhysicsWorld(new Vector2(0,
-				2 * SensorManager.GRAVITY_EARTH), false);
-
+		this.physicsWorld = new PhysicsWorld(new Vector2(0, 2 * SensorManager.GRAVITY_EARTH), false);
+		
 		final Scene scene = new Scene(2);
 		scene.setBackground(new ColorBackground(0, 0, 0));
-
+		
 		// Add blocks
 		for (int i = 0; i < BLOCKS; i++) {
-			float x = 94 + i * 60;
+			float x = 68 + i * 52;
 			
 			// Create block
-			TiledSprite block = new TiledSprite(x, 144,
-					this.tileRegions.clone());
+			TiledSprite block = new TiledSprite(x, 144, this.tileRegions.clone());
 			block.setCurrentTileIndex(i);
 			block.setUpdatePhysics(false);
 			scene.getTopLayer().addEntity(block);
-
+			
 			TiledTextureRegion anchorRegion = this.tileRegions.clone();
 			anchorRegion.setCurrentTileIndex(BLOCKS);
-
+			
 			// Create anchor
 			TiledSprite anchor = new TiledSprite(x, 10, anchorRegion);
 			anchor.setUpdatePhysics(false);
 			scene.getTopLayer().addEntity(anchor);
-
+			
 			// Create block body
-			Body blockBody = PhysicsFactory.createBoxBody(physicsWorld, block,
-					BodyType.DynamicBody, fixtureDefinition);
-			this.physicsWorld.registerPhysicsConnector(new PhysicsConnector(
-					block, blockBody, true, true, false, false));
-
+			Body blockBody = PhysicsFactory.createBoxBody(physicsWorld, block, BodyType.DynamicBody, fixtureDefinition);
+			this.physicsWorld.registerPhysicsConnector(new PhysicsConnector(block, blockBody, true, true, false, false));
+			
 			// Create anchor body
-			Body anchorBody = PhysicsFactory.createBoxBody(physicsWorld,
-					anchor, BodyType.StaticBody, fixtureDefinition);
-			this.physicsWorld.registerPhysicsConnector(new PhysicsConnector(
-					anchor, anchorBody));
-
+			Body anchorBody = PhysicsFactory.createBoxBody(physicsWorld, anchor, BodyType.StaticBody, fixtureDefinition);
+			this.physicsWorld.registerPhysicsConnector(new PhysicsConnector(anchor, anchorBody));
+			
 			// Create joint
 			final RevoluteJointDef revoluteJointDef = new RevoluteJointDef();
-			revoluteJointDef.initialize(anchorBody, blockBody,
-					anchorBody.getWorldCenter());
-
+			revoluteJointDef.initialize(anchorBody, blockBody, anchorBody.getWorldCenter());
+			
 			physicsWorld.createJoint(revoluteJointDef);
 		}
-
+		
 		scene.registerUpdateHandler(this.physicsWorld);
 		return scene;
 	}
-
+	
 	@Override
-	public void onAccelerometerChanged(
-			final AccelerometerData pAccelerometerData) {
-		this.physicsWorld.setGravity(new Vector2(4 * pAccelerometerData.getY(),
-				4 * pAccelerometerData.getX()));
+	public void onAccelerometerChanged(final AccelerometerData pAccelerometerData) {
+		this.physicsWorld.setGravity(new Vector2(4 * pAccelerometerData.getY(), 4 * pAccelerometerData.getX()));
 	}
-
+	
 	@Override
 	public void onLoadComplete() {
 	}
-
+	
 }
